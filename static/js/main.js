7fc18042f8caccbbfb6cc2e2150a827ae3f01b58
ederivc <61289461+ederivc@users.noreply.github.com>
2020-07-22 16:38:51 -0500

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,16 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-const links = document.querySelectorAll('.nav-links li');
+const bdelete = document.querySelectorAll(".btn-delete")
 
-hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-      link.classList.toggle("fade");
+if(bdelete){
+    const myArray = Array.from(bdelete);
+    myArray.forEach((btn) =>{
+        btn.addEventListener("click", (e) => {
+            if(!confirm("The user will be deleted")){
+                e.preventDefault();
+            }
+        });
     });
-  });
+}
 
+function filesize(file){
+    document.cookie = `filesize=${file.files[0].size}`;
+}
